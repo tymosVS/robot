421980b826bfef5f64ee2cc69cc6221ec84c070e
Vladimir <timos9vs@gmail.com>
2019-07-02 13:32:29 +0300

    split method run cli

--- robot_run.rb
+++ robot_run.rb
@@ -4,34 +4,42 @@
 module RobotRun
   load 'robot.rb'
   load 'table.rb'
-  def self.cli_r
+  def self.geted_size?(len, widh)
+    len.size.positive? && widh.size.positive? && widh.to_i <= 0 && len.to_i <= 0
+  end
+
+  def self.create_table
     puts 'Введите длину стола'
-    x = gets.chomp
+    x = gets.chomp.to_i
     puts 'Введите ширину стола'
-    y = gets.chomp
+    y = gets.chomp.to_i
 
-    tabl = if x.size.positive? && y.size.positive? && y.to_i <= 0 && x.to_i <= 0
-             Table::Table.new
-           else
-             Table::Table.new(x.to_i, y.to_i)
-           end
+    tabl = geted_size?(x, y) ? Table::Table.new : Table::Table.new(x, y)
+    tabl
+  end
+
+  def self.choise_action(act, table, robot)
+    case act.split[0]
+    when 'PLACE'
+      act.slice!('PLACE')
+      tmp = act.split(',')
+      robot.place(table, tmp[0].to_i, tmp[1].to_i, tmp[2])
+    when 'MOVE' then robot.move
+    when 'LEFT' then robot.left
+    when 'RIGHT' then robot.right
+    when 'REPORT' then robot.report
+    end
+  end
+
+  def self.cli_r
+    tabl = create_table
     robot = Robot::Robot.new
-
     loop do
       puts 'Выберите действие или введите exit  для выхода'
-      print ['PLACE ', 'MOVE', 'LEFT', 'RIGHT', 'REPORT']
-      puts ''
       act = gets.chomp.upcase
       case act.split[0]
-      when 'PLACE'
-        act.slice!('PLACE')
-        tmp = act.gsub(/\s+/, '').split(',')
-        robot.place(tabl, tmp[0].to_i, tmp[1].to_i, tmp[2])
-      when 'MOVE' then robot.move
-      when 'LEFT' then robot.left
-      when 'RIGHT' then robot.right
-      when 'REPORT' then robot.report
       when 'EXIT' then break
+      else choise_action(act, tabl, robot)
       end
     end
   end
@@ -51,7 +59,6 @@ module RobotRun
           tmp = act.gsub(/\s+/, '').split(',')
           tab_f = Table::Table.new(tmp[0].to_i, tmp[1].to_i)
         when 'PLACE'
-          print line
           act.slice!('PLACE')
           tmp = act.gsub(/\s+/, '').split(',')
           robot.place(tab_f, tmp[0].to_i, tmp[1].to_i, tmp[2])
@@ -59,7 +66,6 @@ module RobotRun
         when 'LEFT' then robot.left
         when 'RIGHT' then robot.right
         when 'REPORT' then robot.report
-        when 'EXIT' then break
         end
       end
     else
